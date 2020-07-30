84c8c8668d0685b2077023d996135ac670179cae
Anthony Wang <ta180m@gmail.com>
2020-07-30 01:59:39 +0000

    Add files via upload

---
+++ optimize.ps1
@@ -0,0 +1,4 @@
+Optimize-VHD "C:\Users\Public\Documents\Hyper-V\Virtual hard disks\Ubuntu 20.04.vhdx"
+Optimize-VHD $env:USERPROFILE\AppData\Local\Docker\wsl\data\ext4.vhdx
+Optimize-VHD $env:USERPROFILE\AppData\Local\Packages\CanonicalGroupLimited.UbuntuonWindows_79rhkp1fndgsc\LocalState\ext4.vhdx
+Optimize-VHD $env:USERPROFILE\AppData\Local\Packages\TheDebianProject.DebianGNULinux_76v4gfsz19hv4\LocalState\ext4.vhdx
