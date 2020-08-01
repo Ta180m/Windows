424df87c0dbf5b6cbd90111f12d7c60e5a055895
Anthony Wang <ta180m@gmail.com>
2020-08-01 18:17:01 -0500

    Update reverse.ps1

--- reverse.ps1
+++ reverse.ps1
@@ -1,1 +1,1 @@
-sudo { Get-ItemProperty HKLM:\SYSTEM\CurrentControlSet\Enum\HID\*\*\Device` Parameters FlipFlopWheel -EA 0 | ForEach-Object { Set-ItemProperty $_.PSPath FlipFlopWheel 1 } }
+sudo Get-ItemProperty HKLM:\SYSTEM\CurrentControlSet\Enum\HID\*\*\Device` Parameters FlipFlopWheel -EA 0 | ForEach-Object { Set-ItemProperty $_.PSPath FlipFlopWheel 1 }
