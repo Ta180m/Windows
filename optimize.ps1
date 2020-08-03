4b6d19f1ec916d67271e84d138ab44a6170d5037
Anthony Wang <ta180m@gmail.com>
2020-08-03 11:15:10 -0500

    Delete optimize.ps1

--- optimize.ps1
+++
@@ -1,3 +0,0 @@
-Optimize-VHD $env:USERPROFILE\AppData\Local\Docker\wsl\data\ext4.vhdx
-Optimize-VHD $env:USERPROFILE\AppData\Local\Packages\CanonicalGroupLimited.UbuntuonWindows_79rhkp1fndgsc\LocalState\ext4.vhdx
-Optimize-VHD $env:USERPROFILE\AppData\Local\Packages\TheDebianProject.DebianGNULinux_76v4gfsz19hv4\LocalState\ext4.vhdx
